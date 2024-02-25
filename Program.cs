8877057d8ba7148be4ce9d538e28437f8c329dd4
eduardo2580 <118479773+eduardo2580@users.noreply.github.com>
2024-02-25 19:03:09 -0300

    Day 1

--- Program.cs
+++ Program.cs
@@ -1,105 +1,161 @@
-﻿namespace CommandLineGame;
+﻿using System;
+using System.Linq;
 
-public static class Program {
-    private const int Rows = 10;
-    private const int Cols = 30;
-    private static char[,] _grid = new char[Rows, Cols];
-    private static int _coins;
+namespace CommandLineGame
+{
+    public static class Program
+    {
+        private const int Rows = 10;
+        private const int Cols = 30;
+        private static char[,] _grid;
+        private static int _coins;
 
-    private const char ObstacleChar = '#';
-    private const char CoinChar = '*';
-    private const char PlayerChar = 'x';
-    private const char WallChar = '0';
-    private const char TileChar = '_';
+        private const char ObstacleChar = '#';
+        private const char CoinChar = '*';
+        private const char PlayerChar = 'x';
+        private const char WallChar = '0';
+        private const char TileChar = '_';
 
-    public static void Main() {
-        Console.Clear();
-        while (Run()) { }
-        Console.WriteLine("\nBBye!");
-    }
+        public static void Main()
+        {
+            Console.Clear();
+            while (Run()) { }
+            Console.WriteLine("\nBBye!");
+        }
 
-    private static bool Run() {
-        var player = (X: 1, Y: 1);
-        
-        _grid = new char[Rows, Cols];
-        _coins = 0;
+        private static bool Run()
+        {
+            InitializeGame();
 
-        // Populate grid
-        for (var x = 0; x < Cols; x++) {
-            for (var y = 0; y < Rows; y++) {
-                if (x == 0 || x == Cols - 1 || y == 0 || y == Rows - 1) {
-                    _grid[y, x] = WallChar; continue;
+            var player = (X: 1, Y: 1);
+
+            while (true)
+            {
+                Console.Clear();
+                RenderGame();
+
+                if (CheckWinCondition())
+                {
+                    Console.WriteLine("\nYOU WIN!\nPRESS R TO RESTART OR ESC TO QUIT.");
+                    while (true)
+                    {
+                        var key = Console.ReadKey();
+                        if (key.Key == ConsoleKey.R) return true;
+                        if (key.Key == ConsoleKey.Escape) return false;
+                    }
                 }
-                _grid[y, x] = TileChar;
+
+                HandleUserInput(ref player);
             }
         }
 
-        // Place boxes and coins
-        var rnd = new Random();
-        var boxes = Enumerable.Range(0, 10).Select(_ => (X: rnd.Next(2, Cols-2), Y: rnd.Next(2,Rows-2))).ToArray();
-        foreach (var box in boxes) { _grid[box.Y, box.X] = ObstacleChar; }
-        var coinsPositions = Enumerable.Range(0, 5).Select(_ => (X: rnd.Next(1, Cols-1), Y: rnd.Next(1, Rows-1))).ToArray();
-        foreach (var coin in coinsPositions) { _grid[coin.Y, coin.X] = CoinChar; }
+        private static void InitializeGame()
+        {
+            _grid = new char[Rows, Cols];
+            _coins = 0;
 
-        // Place the player
-        _grid[player.Y, player.X] = PlayerChar;
+            // Populate grid
+            for (var x = 0; x < Cols; x++)
+            {
+                for (var y = 0; y < Rows; y++)
+                {
+                    if (x == 0 || x == Cols - 1 || y == 0 || y == Rows - 1)
+                    {
+                        _grid[y, x] = WallChar; continue;
+                    }
+                    _grid[y, x] = TileChar;
+                }
+            }
 
-        while (true) {
-            Console.Clear();
-            Console.WriteLine($"Welcome to this anonymous Sokoban-like game!\n\nCoins: {_coins}\n");
+            // Place boxes and coins
+            PlaceObstacles();
+            PlaceCoins();
+        }
 
-            // Update the grid
-            for (var i = 0; i < _grid.GetLength(0); i++) {
-                for (var j = 0; j < _grid.GetLength(1); j++) {
+        private static void PlaceObstacles()
+        {
+            var rnd = new Random();
+            var boxes = Enumerable.Range(0, 10).Select(_ => (X: rnd.Next(2, Cols - 2), Y: rnd.Next(2, Rows - 2))).ToArray();
+            foreach (var box in boxes) { _grid[box.Y, box.X] = ObstacleChar; }
+        }
+
+        private static void PlaceCoins()
+        {
+            var rnd = new Random();
+            var coinsPositions = Enumerable.Range(0, 5).Select(_ => (X: rnd.Next(1, Cols - 1), Y: rnd.Next(1, Rows - 1))).ToArray();
+            foreach (var coin in coinsPositions) { _grid[coin.Y, coin.X] = CoinChar; }
+        }
+
+        private static void RenderGame()
+        {
+            Console.WriteLine("Welcome to this Sokoban-like terminal game!");
+            Console.WriteLine($"Coins collected: {_coins}\n");
+
+            for (var i = 0; i < _grid.GetLength(0); i++)
+            {
+                for (var j = 0; j < _grid.GetLength(1); j++)
+                {
                     Console.Write(_grid[i, j]);
                 }
                 Console.Write("\n");
             }
 
-            // Check winning condition
-            if (_coins == coinsPositions.Length) {
-                Console.WriteLine("\nYOU WIN!\nPRESS R TO RESTART OR ESC TO QUIT.");
-                while (true)
-                {
-                    var key = Console.ReadKey();
-                    if (key.Key == ConsoleKey.R) return true;
-                    if (key.Key == ConsoleKey.Escape) return false;
-                }
-            }
-            
-            // elaborate user input
             Console.WriteLine("\nArrow keys => move the player\nEscape => exit.");
+        }
+
+        private static void HandleUserInput(ref (int X, int Y) player)
+        {
             var input = Console.ReadKey();
-            switch (input.Key) {
-                case ConsoleKey.Escape: return false;
-                case ConsoleKey.UpArrow: Move(0, -1, ref player); break;
-                case ConsoleKey.DownArrow: Move(0, 1, ref player); break;
-                case ConsoleKey.LeftArrow: Move(-1, 0, ref player); break;
-                case ConsoleKey.RightArrow: Move(1, 0, ref player); break;
-                case ConsoleKey.R: return true;
+            switch (input.Key)
+            {
+                case ConsoleKey.Escape:
+                    Environment.Exit(0);
+                    break;
+                case ConsoleKey.UpArrow:
+                    Move(0, -1, ref player);
+                    break;
+                case ConsoleKey.DownArrow:
+                    Move(0, 1, ref player);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    Move(-1, 0, ref player);
+                    break;
+                case ConsoleKey.RightArrow:
+                    Move(1, 0, ref player);
+                    break;
+                case ConsoleKey.R:
+                    InitializeGame();
+                    break;
             }
         }
-    }
-    
-    private static void Move(int x, int y, ref (int X, int Y) player) {
-        var to = new { X = player.X + x, Y = player.Y + y };
-        if (to.X <= 0 || to.Y <= 0 || to.X >= Cols - 1 || to.Y >= Rows - 1) return;
 
-        if (_grid[to.Y, to.X] == ObstacleChar)
+        private static void Move(int x, int y, ref (int X, int Y) player)
         {
-            var obstacleTo = new { X = to.X + x, Y = to.Y + y };
-            if (obstacleTo.X <= 0 || obstacleTo.Y <= 0 || 
-                obstacleTo.X >= Cols - 1 || obstacleTo.Y >= Rows - 1 ||
-                _grid[obstacleTo.Y, obstacleTo.X] == ObstacleChar ||
-                _grid[obstacleTo.Y, obstacleTo.X] == CoinChar) return;
-            _grid[obstacleTo.Y, obstacleTo.X] = ObstacleChar;
+            var to = new { X = player.X + x, Y = player.Y + y };
+            if (to.X <= 0 || to.Y <= 0 || to.X >= Cols - 1 || to.Y >= Rows - 1) return;
+
+            if (_grid[to.Y, to.X] == ObstacleChar)
+            {
+                var obstacleTo = new { X = to.X + x, Y = to.Y + y };
+                if (obstacleTo.X <= 0 || obstacleTo.Y <= 0 ||
+                    obstacleTo.X >= Cols - 1 || obstacleTo.Y >= Rows - 1 ||
+                    _grid[obstacleTo.Y, obstacleTo.X] == ObstacleChar ||
+                    _grid[obstacleTo.Y, obstacleTo.X] == CoinChar) return;
+                _grid[obstacleTo.Y, obstacleTo.X] = ObstacleChar;
+            }
+
+            if (_grid[to.Y, to.X] == CoinChar) _coins++;
+
+            _grid[player.Y, player.X] = TileChar;
+            _grid[to.Y, to.X] = PlayerChar;
+
+            player.X = to.X; player.Y = to.Y;
         }
 
-        if (_grid[to.Y, to.X] == CoinChar) _coins++;
-
-        _grid[player.Y, player.X] = TileChar;
-        _grid[to.Y, to.X] = PlayerChar;
-
-        player.X = to.X; player.Y =to.Y;
+        private static bool CheckWinCondition()
+        {
+            return _coins == Enumerable.Range(0, 5).Count();
+        }
     }
 }
+
--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-# sokoban-like-terminal-game
-A simple, [sokoban-like](https://it.wikipedia.org/wiki/Sokoban), terminal-based game.
+# Sokoban-like Terminal Game
 
-![image](https://github.com/MammaMiaDev/sokoban-like-terminal-game/assets/140280885/d103cc4f-c88b-4228-bb5b-418cf37b2f69)
+A simple, Sokoban-like terminal-based game written in C#.
+
+## Overview
+
+This project is a basic implementation of a Sokoban-like game that you can play in the terminal. The objective is to move the player character (`x`) and collect all the coins (`*`) while avoiding obstacles (`#`).
+
+## Features
+
+- Terminal-based gameplay.
+- Color-coded elements for better visualization.
+- Simple controls using arrow keys.
+- Restart the game option.
+
+## How to Play
+
+1. Clone the repository to your local machine.
+2. Compile and run the program using a C# compiler.
+3. Use arrow keys to navigate the player (`x`) in the terminal.
+4. Collect all the coins (`*`) to win the game.
+5. Press 'R' to restart the game and 'ESC' to quit.
+
+## Dependencies
+
+- .NET Core SDK
+
+## Screenshots
+
+(Add any relevant screenshots if needed)
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgments
+
+- The inspiration for this game comes from the classic game [Sokoban](https://en.wikipedia.org/wiki/Sokoban).
+
+Feel free to modify this README to better suit your project and provide more details about your game. If you have any additional sections or information you'd like to include, please customize it accordingly.
+
